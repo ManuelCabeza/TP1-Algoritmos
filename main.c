5dbf09647ca5c5a09d98304230477d6e15faa138
Manuel Cabeza <trabajosmanu1@gmail.com>
2018-10-10 21:39:35 -0300

    Empiezo con las funciones de procesar NMEA, todavia no hay nada. Nada en el main

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdib.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
 
+
+int main(int argc, char * argv[]) {
+	
+	//trabajar con los comandos 
+	
+	//llamar a funcion en procesarNMEA
+	
+}
--- procesarNMEA.c
+++ procesarNMEA.c
@@ -1,1 +1,15 @@
+#include "verificacionesprocesarNMEA.h"
+
+#define MAX_LONG_SEN 250
+
+void procesarNMEA() {
+	
+	char sentencia[MAX_LONG_SEN]; // Max longitud de un string posible
+	char * sentenciaptr; //por ahora no se usa 10/10
+	
+	fgets(sentencia, MAX_LONG_SEN,stdin);
+	
+	verificarargumentos(sentencia);
+	
+}
 
