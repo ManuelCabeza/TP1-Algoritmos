3c1967d7125e862a282935855ecab2898a0f0028
Manuel Cabeza <trabajosmanu1@gmail.com>
2018-10-12 17:02:49 -0300

    Hora todavia no, el resto todo procesado y bien

--- procesarNMEA.c
+++ procesarNMEA.c
@@ -8,43 +8,11 @@
  * W multiplica por -1 longitud
  * Calidad del fix numero del 0 al 8 discreto
  * Cantidad de satélites, número del 0 a 12 discreto
- * HDop debe ser 1.0 ? Preguntar a Pato .. Rta Debe ser numero
- * Elevación debe ser 0.0 ? Preguntar a Pato .. Rta Debe ser numero
+ * HDop debe ser 1.0 ? Preguntar a Pato .. Rta: Debe ser numero
+ * Elevación debe ser 0.0 ? Preguntar a Pato .. Rta: Debe ser numero
  * Unidad Las dos deben ser M
  * Suma de verificacion 
  */
- 
-//#include "verificacionesprocesarNMEA.h"
-
-#define MAX_LONG_SEN 85
-#define CANT_ARGUMENTOS 14
-#define MAX_VALOR_FIX 8
-#define MIN_VALOR_FIX 0
-#define MAX_VALOR_CANT_SATELITES 12
-#define MIN_VALOR_CANT_SATELITES 0
-#define CARACTER_NORTE 'n'
-#define CARACTER_SUR   's'
-#define CARACTER_ESTE  'e'
-#define CARACTER_OESTE 'w'
-#define CARACTER_UNIDAD 'm'
-#define CARACTER_INICIO_COMANDO '$' 
-#define CARACTER_SEPARACION_COMANDO ','
-#define CARACTER_SUMA_VER '*'
-
-typedef enum {PR_OK, PR_ERR, PR_FIN} procesar_t;
-
-typedef struct {
-	float horario;
-	float latitud;
-	float longitud;
-	int calidad_fix;
-	int cant_satelites;
-	float hdop;
-	float elevacion;
-	float sep_geo;
-} gga;
-
-unsigned char nmea_verificar_suma(const char * sentencia);
 
 /*La funcion devuelve:
  * PR_FIN si no hay nada mas que leer en el archivo
@@ -99,7 +67,7 @@ procesar_t procesarNMEA(gga * ggaptr) {
 		return PR_ERR;
 	
 	//Asigna valor de longitud a partir de formato dddmm.mmm	
-	ggaptr->longitud = (((int)longitud / 100) + ((longitud - 100 * ((int)longitud / 100)) / 60 )) * (c == CARACTER_SUR ? -1 : 1); 
+	ggaptr->longitud = (((int)longitud / 100) + ((longitud - 100 * ((int)longitud / 100)) / 60 )) * (c == CARACTER_OESTE ? -1 : 1); 
 	
 	//Verifica que el sexto argumento sea un numero entero entre 0 y 8
 	if ((ggaptr->calidad_fix = strtol(++strptr, &strptr, 10)) < MIN_VALOR_FIX || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO) || (ggaptr->calidad_fix) > MAX_VALOR_FIX) 
@@ -141,8 +109,6 @@ procesar_t procesarNMEA(gga * ggaptr) {
 	if (nmea_verificar_suma( strptr) != suma_verificacion)
 		return PR_ERR;
 	
-	puts("Esta bien");
-		
 	return PR_OK;
 }
 
@@ -156,3 +122,4 @@ unsigned char nmea_verificar_suma(const char * sentencia) {
 		
 	return suma;
 }
+
--- procesarNMEA.h
+++ procesarNMEA.h
@@ -13,8 +13,24 @@
 #define CARACTER_SEPARACION_COMANDO ','
 #define CARACTER_SUMA_VER '*'
 
+typedef struct {
+	float horario;
+	float latitud;
+	float longitud;
+	int calidad_fix;
+	int cant_satelites;
+	float hdop;
+	float elevacion;
+	float sep_geo;
+} gga;
 
 typedef enum {PR_OK, PR_ERR, PR_FIN} procesar_t;
 
+//Funcion que recibe una sentencia y calcula la XOR de todos los bytes hasta llegar a un caracter de corte
 unsigned char nmea_verificar_suma(const char * sentencia);
+
+/*La funcion devuelve:
+ * PR_FIN si no hay nada mas que leer en el archivo
+ * PR_ERR si el formato de una linea de datos no esta bien
+ * PR_OK si el dato de una linea fue procesado correctamente. */
 procesar_t procesarNMEA(gga * ggaptr);
