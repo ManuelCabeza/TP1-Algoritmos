bb9024ecc3b372f6c6eb1bbabc54c9f09666564b
Manuel Cabeza <trabajosmanu1@gmail.com>
2018-10-11 23:39:35 -0300

    Se modifican estos archivos (No son necesarios por ahora)

--- verificacionesprocesarNMEA.h
+++ verificacionesprocesarNMEA.h
@@ -1,8 +1,1 @@
-#include "verificacionesprocesarNMEA.c"
-
-bool verificarprimerchar(char sentencia[]);
-bool verificanumeroconpunto(char sentencia[]);
-bool verificarchar(char sentencia[], char caracter1, char caracter2);
-bool verificarcharunidad(char sentencia[], char caracter);
-bool verificarintervalo(char sentencia[], int minimo, int maximo);
 
