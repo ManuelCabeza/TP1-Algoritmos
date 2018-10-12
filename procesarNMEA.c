d10f0f7a8f488a9ed6244d0e9ea8d11000fe1fe9
Manuel Cabeza <trabajosmanu1@gmail.com>
2018-10-11 23:37:58 -0300

    Procesamiento NMEA, ahora con punteros! (Sin StrTok como dios manda)

--- procesarNMEA.c
+++ procesarNMEA.c
@@ -8,16 +8,15 @@
  * W multiplica por -1 longitud
  * Calidad del fix numero del 0 al 8 discreto
  * Cantidad de satélites, número del 0 a 12 discreto
- * HDop debe ser 1.0 ? Preguntar a Pato
- * Elevación debe ser 0.0 ? Preguntar a Pato
+ * HDop debe ser 1.0 ? Preguntar a Pato .. Rta Debe ser numero
+ * Elevación debe ser 0.0 ? Preguntar a Pato .. Rta Debe ser numero
  * Unidad Las dos deben ser M
  * Suma de verificacion 
  */
  
-#include "verificacionesprocesarNMEA.h"
+//#include "verificacionesprocesarNMEA.h"
 
-#define MAX_LONG_SEN 250
-#define CARACTER_SEPARACION_COM "," //COM de COMANDOS
+#define MAX_LONG_SEN 85
 #define CANT_ARGUMENTOS 14
 #define MAX_VALOR_FIX 8
 #define MIN_VALOR_FIX 0
@@ -28,67 +27,123 @@
 #define CARACTER_ESTE  'e'
 #define CARACTER_OESTE 'w'
 #define CARACTER_UNIDAD 'm'
+#define CARACTER_INICIO_COMANDO '$' 
+#define CARACTER_SEPARACION_COMANDO ',' 
+
+typedef enum {PR_FIN, PR_OK, PR_ERR} procesar_t;
 
 
-bool procesarNMEA() {
+procesar_t procesarNMEA() {
 	
-	int i = 1;
-	char sentencia[MAX_LONG_SEN]; // Max longitud de un string posible
-	//char * sentenciaptr; //por ahora no se usa fecha 10/10
-	char * argumentos[CANT_ARGUMENTOS]; //En cada casilla de argumentos hay un puntero que apunta a su argumento. HASTA ANTES DE LOS CAMPOS NULOS
+	char c; //Variable auxiliar para no usar tolower mas de 2 veces.
+	char string[MAX_LONG_SEN]; // Max longitud de un string posible
+	char * strptr;
 	
-	fgets(sentencia, MAX_LONG_SEN,stdin);
 	
-	argumentos[0] = strtok(sentencia, CARACTER_SEPARACION_COM);
+	int calfix, cantsat;
 	
-	while (argumentos[i] != NULL) {
-		argumentos[i] = strtok(NULL, CARACTER_SEPARACION_COM);
-		i++;
+	float horario, HDoP, elvacion, sepgeo;
+	float longitud;
+	float latitud;
+	
+	if (fgets(string, MAX_LONG_SEN,stdin) == NULL) 
+		return PR_FIN;
+	
+	strptr = string;
+	if (* strptr != CARACTER_INICIO_COMANDO) {
+		puts("Caracter incorrecto");
+		return PR_ERR;
 	}
 	
-	for (i = 0; i < CANT_ARGUMENTOS - 1; i++)
-		printf("%s\n", argumentos[i]);
-	
-	
-	if (verificarprimerchar(argumentos[0]) != true)
-		return false;
-	
-	if (verificarnumeroconpunto(argumentos[1]) != true) //Para la hora
-		return false;
-	
-	if (verificarnumeroconpunto(argumentos[2]) != true) //Para la latitud
-		return false;
-	
-	//cargarlatitud(argumentos[2], estructura ??); HAY QUE CARGAR TODOS
-	
-	if (verificarchar(argumentos[3], CARACTER_NORTE, CARACTER_SUR) != true)
-		return false;
-	
-	if (verificarnumeroconpunto(argumentos[4]) != true) //Para la longitud
-		return false;
-	
-	if (verificarchar(argumentos[5], CARACTER_OESTE, CARACTER_ESTE) != true)
-		return false;
-	
-	if(verificarintervalo(argumentos[6], MIN_VALOR_FIX, MAX_VALOR_FIX) != true)  //Verifica el valor del Fix
-		return false;
-	
-	if(verificarintervalo(argumentos[7], MIN_VALOR_CANT_SATELITES, MAX_VALOR_CANT_SATELITES) != true) { //Verifica el valor del Fix
-		puts("MAl intervalo2"); //Esto esta mal preguntar como hacer al grupo
-		return false;
+	if ((strptr = strchr(string, CARACTER_SEPARACION_COMANDO)) != NULL) {
+		strptr++;
+		if ((horario = strtof(strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO)) {
+			puts("Tiene horario invalido");
+			return PR_ERR;
+		}
 	}
-	if(verificarcharunidad(argumentos[10], CARACTER_UNIDAD) != true) {
-		puts("MALUNIDAD");
-		return false;
+	
+	printf("|%c %c %c|\n", *(strptr - 1), *strptr, *(strptr + 1));
+	
+	if ((longitud = strtof(strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO)) {
+			puts("Tiene longitud invalida");
+			return PR_ERR;
 	}
-	if(verificarcharunidad(argumentos[12], CARACTER_UNIDAD) != true) {
-		puts("MALUNIDAD");
-		return false;
+	
+	printf("|%c %c %c|\n", *(strptr - 1), *strptr, *(strptr + 1));
+	
+	c = tolower( * (strptr++));
+	if ((c != CARACTER_NORTE) && (c != CARACTER_SUR)){
+		puts("Tiene norte sur mal");
+		return PR_ERR;
 	}
+	
+	printf("|%c %c %c|\n", *(strptr - 1), *strptr, *(strptr + 1));
+	
+	if ((latitud = strtof(++strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO)) {
+			puts("Tiene latitud invalida");
+			return PR_ERR;
+	}
+	
+	printf("|%c %c %c|\n", *(strptr - 1), *strptr, *(strptr + 1));
+	
+	c = tolower( * (strptr++));
+	if ((c != CARACTER_ESTE) && (c != CARACTER_OESTE)){
+		puts("Tiene oeste este mal");
+		return PR_ERR;
+	}
+	strptr++;
+	printf("|%c %c %c|\n", *(strptr - 1), *strptr, *(strptr + 1));
+
+	if ((calfix = strtof(++strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO)) {
+			puts("Tiene calfix invalida");
+			return PR_ERR;
+	}	
+	
+	printf("|%c %c %c|\n", *(strptr - 1), *strptr, *(strptr + 1));
+	
+	if ((cantsat = strtof(++strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO)) {
+			puts("Tiene cansat invalida");
+			return PR_ERR;
+	}
+	
+	printf("|%c %c %c|\n", *(strptr - 1), *strptr, *(strptr + 1));
+	
+	if ((HDoP = strtof(++strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO)) {
+			puts("Tiene HDoP invalida");
+			return PR_ERR;
+	}
+	
+	printf("|%c %c %c|\n", *(strptr - 1), *strptr, *(strptr + 1));
+	
+	if ((elvacion = strtof(++strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO)) {
+			puts("Tiene elevacion invalida");
+			return PR_ERR;
+	}
+	
+	printf("|%c %c %c|\n", *(strptr - 1), *strptr, *(strptr + 1));
+	
+	if (tolower(* strptr++) != CARACTER_UNIDAD){
+		puts("Tiene unidad 1 mal");
+		return PR_ERR;
+	}
+	
+	printf("|%c %c %c|\n", *(strptr - 1), *strptr, *(strptr + 1));
+	
+	if ((sepgeo = strtof(++strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO)) {
+			puts("Tiene sepgeo invalida");
+			return PR_ERR;
+	}
+
+	printf("|%c %c %c|\n", *(strptr - 1), *strptr, *(strptr + 1));
+
+	if (tolower(* strptr) != CARACTER_UNIDAD){
+		puts("Tiene unidad 2 mal");
+		return PR_ERR;
+	}
+	
+	
+	puts("Esta bien");
 		
-		
-		
-		
-	puts("Bueno");
-	return true;
+	return PR_OK;
 }
