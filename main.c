6953221fcc2d2f435c0f6c5a1dd3a6e7393ecb6b
Tamara <tamaraespejoruiz@gmail.com>
2018-10-13 00:45:21 -0300

    La funcion procesar_argumentos hace bien solamente imprimir_ayuda, falta analizar en profundidad los demas casos

--- main.c
+++ main.c
@@ -6,46 +6,28 @@
 
 #include "verificaciones_main.h"
 
-
-//No se me ocurren nombres de variables mas representativos.
-
-#define ARG_VALIDO_AYUDA "-h"
-#define ARG_VALIDO_AYUDA_V "--help"
-#define ARG_VALIDO_NOMBRE "-n"
-#define ARG_VALIDO_NOMBRE_V "--name"
-#define ARG_VALIDO_FECHA "-f" //Si no se indica fecha se toma la del sistema
-#define ARG_VALIDO_FECHA_V "--format"
-#define ARG_VALIDO_ANIO "-Y"
-#define ARG_VALIDO_ANIO_V "--year"
-#define ARG_VALIDO_MES "-m"
-#define ARG_VALIDO_MES_V "--month"
-#define ARG_VALIDO_DIA "-d"
-#define ARG_VALIDO_DIA_V "--day"
-
-#define MAX_CANT_ARG 12 //num del enum arg_t
-#define MSJ_IMPRIMIR_AYUDA "Ayuda"
-
-
 int main(int argc, char *argv[]) {
 
 	char nombre[100] = {0};
 	int fecha;
-	//Va en la estructura
+	//Va en la gpx
 
 	status_t st;
-
-	
 	st = procesar_argumentos(argc, argv, nombre, &fecha);
 	
-	if (st == ST_PEDIR_AYUDA)
-		imprimir_ayuda(ST_PEDIR_AYUDA);
-
-	//VER COMO ANALIZAR LOS DISTINTOS ERRORES
-	if (st != ST_OK && st != ST_PEDIR_AYUDA) {  
-		fprintf(stderr, "%s\n", MSJ_ERROR_PREFIJO);
+	if (st == ST_PEDIR_AYUDA) { 
+		imprimir_ayuda();
 		return EXIT_FAILURE;
 	}
 
+	//VER COMO ANALIZAR LOS DISTINTOS ERRORES
+	if (st != ST_OK && st != ST_PEDIR_AYUDA) {  
+		imprimir_errores(st);
+		return EXIT_FAILURE;
+	}
+
+	printf("%s\n", nombre);
+	printf("%d\n", fecha);
 
 	
 	//llamar a funcion en procesarNMEA
@@ -65,48 +47,44 @@ status_t procesar_argumentos(int argc, char *argv[], char *nombre, int *fecha) {
 	int i, j;
 	char *perr = NULL;
 
-	if (nombre == NULL|| !argv||!nombre|| !fecha)
+	if (nombre == NULL|| !argv|| !nombre|| !fecha)
 		return ST_ERROR_PUNTERO_NULO;
 	
-	//Aca pongo parametros por omision, para cuando tenga la estructura
+	//Aca pongo parametros por omision, para procesargpx
 	//hacerlo en una funcion aparte que se llame cargar_argumentos_por_omision;
 	strcpy(nombre, "tu mama");
 	*fecha = 20181011;
-	
-
 
 	for (i = 1; i < argc ; i++) {
 		for (j = 0; j < MAX_CANT_ARG; j++) {
-			if (!strcmp(argv[i], arg_validos[j]))
-				//ACA EL SWITCH
-				break; //Ya encontre lo que buscaba ya que j es el argumento
+			if (strcmp(argv[i], arg_validos[j]) == 0) {
+				j = j/2;
+
+				switch (j) {
+					case ARG_AYUDA: 
+						return ST_PEDIR_AYUDA;
+						break;
+					case ARG_NOMBRE:
+						//Guarda el nombre en la GPX
+						i++;
+						validar_argumento_nombre(argv[i], nombre);
+						//Tengo que verificar que todos esten bien CUIDADO
+						break;
+					case ARG_FECHA:
+						i++; 
+						validar_argumento_fecha(argv[i], fecha);
+						break;
+					case ARG_ANIO:
+						break;
+					case ARG_MES:
+						break;
+					case ARG_DIA:
+						break;
+				}
+
+			} 
+				//break; //Ya encontre lo que buscaba ya que j es el argumento
 		}
-	}
-
-	switch(j) {
-		case ARG_AYUDA:ARG_AYUDA_V: //LE DIGO AL MAIN QUE ME LLEGO UNA AYUDA
-			return ST_PEDIR_AYUDA;
-			break;
-		case ARG_NOMBRE: ARG_NOMBRE_V:
-			//Guarda el nombre en la estructura.
-			i++;
-			strcpy(nombre, argv[i]);
-			return ST_OK; //Tengo que verificar que todos esten bien CUIDADO!
-			break;
-		case ARG_FECHA: ARG_FECHA_V:
-			i++; 
-			*fecha = strtol(argv[i],&perr,10);
-			if (*perr != '\0' || *fecha < 0)
-				return ST_ERROR_FECHA_INVALIDA;
-			return ST_OK;
-			break;
-		case ARG_ANIO: ARG_ANIO_V:
-			break;
-		case ARG_MES: ARG_MES_V:
-			break;
-		case ARG_DIA: ARG_DIA_V:
-			break;
-
 	}
 
 	return ST_OK;
@@ -115,4 +93,37 @@ status_t procesar_argumentos(int argc, char *argv[], char *nombre, int *fecha) {
 
 void imprimir_ayuda() {
 	printf("%s\n", MSJ_IMPRIMIR_AYUDA);
+}
+
+
+void imprimir_errores(status_t estado) {
+
+	switch (estado) {
+		case ST_OK:
+			break;
+		case ST_ERROR_PUNTERO_NULO:
+			fprintf(stderr, "%s : %s\n", MSJ_ERROR_PREFIJO, MSJ_ERROR_PUNTERO_NULO);
+			break;
+		case ST_ERROR_FECHA_INVALIDA: 
+			fprintf(stderr, "%s : %s\n", MSJ_ERROR_PREFIJO, MSJ_ERROR_FECHA_INVALIDA);
+			break;
+		case ST_ERROR_NOMBRE_INVALIDO:
+			fprintf(stderr, "%s : %s\n", MSJ_ERROR_PREFIJO, MSJ_ERROR_NOMBRE_INVALIDO);
+			break;
+
+		case ST_ERROR_SEGUNDO_INVALIDO:
+			fprintf(stderr, "%s : %s\n", MSJ_ERROR_PREFIJO, MSJ_ERROR_SEGUNDO_INVALIDO);
+			break;
+		case ST_ERROR_MINUTO_INVALIDO:
+			fprintf(stderr, "%s : %s\n", MSJ_ERROR_PREFIJO, MSJ_ERROR_MINUTO_INVALIDO);
+			break;
+		case ST_ERROR_HORA_INVALIDA:
+			fprintf(stderr, "%s : %s\n", MSJ_ERROR_PREFIJO, MSJ_ERROR_HORA_INVALIDA);
+			break;
+			
+		case ST_PEDIR_AYUDA:
+			break;
+
+	}
+
 }
