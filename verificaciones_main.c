7e1b642466b3cf1de5d23f0f9b756bcdce3d9d76
Tamara <tamaraespejoruiz@gmail.com>
2018-10-13 00:49:28 -0300

    Estan empezadas las funciones que validan el nombre y fecha. Las demas faltan

--- verificaciones_main.c
+++ verificaciones_main.c
@@ -1,1 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+#include "verificaciones_main.h"
+
+status_t validar_argumento_nombre(char *argv_nombre, char *nombre) {
+
+	if (argv_nombre == NULL || nombre == NULL)
+		return ST_ERROR_PUNTERO_NULO;
+	
+	strcpy(nombre, argv_nombre);
+	return ST_OK;
+	
+}
+
+status_t validar_argumento_fecha(char *argv_fecha, int *fecha) {
+
+	char *perr = NULL;
+	int cant;
+
+	if (argv_fecha == NULL || fecha == NULL)
+		return ST_ERROR_PUNTERO_NULO;
+	
+	cant = strlen(argv_fecha);
+    printf("cant es %d\n", cant);
+	if ( (cant + 1) != CANT_MIN_PALABRA)
+		return ST_ERROR_FECHA_INVALIDA;
+	
+	*fecha = strtol(argv_fecha, &perr, 10);
+
+	if (*perr != '\0' || *fecha < 0)
+		return ST_ERROR_FECHA_INVALIDA;
+	
+	return ST_OK;
+}
+
+//validar_argumento_mes
+//validar_argumento_anio
+//validar_argumento_dia
