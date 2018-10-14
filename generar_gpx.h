444dee1f1705fd2bfd61b89c8214ff6d12daa01c
Manuel Cabeza <trabajosmanu1@gmail.com>
2018-10-14 00:43:57 -0300

    Agrego el archivo generar_gpx.h

---
+++ generar_gpx.h
@@ -0,0 +1,14 @@
+#define MAX_LONG_NOMBRE 100
+
+typedef struct {
+	char nombre[MAX_LONG_NOMBRE];
+	// VER TIME.H para el tiempo
+} metadata;
+
+
+typedef enum { INICIAR, INICIAR_ENTER, FINAL, FINAL_ENTER} tipo_tag;
+
+void generar_gpx(gga * ggaptr, metadata * metptr);
+
+void tag(char * strptr, tipo_tag tipo, size_t indentacion);
+
