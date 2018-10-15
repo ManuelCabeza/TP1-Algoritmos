f5ffa9043b820dba0452297d0eca4027b2de482c
Tamara <tamaraespejoruiz@gmail.com>
2018-10-15 15:17:45 -0300

    Argumento fecha esta validada. Ver como considerar el caso de febrero, si es necesario

--- verificaciones_main.c
+++ verificaciones_main.c
@@ -28,15 +28,27 @@ status_t validar_argumento_nombre(char *argv_nombre, char *nombre) {
 	
 }
 
-status_t validar_argumento_fecha(char *argv_fecha, int *fecha) { 
-//ANALIZAR COMO VALIDAR LOS MES Y DIA EN FECAH
+status_t validar_argumento_fecha(char *argv_fecha, int *fecha, metadata *datos_usuario) { 
 //validar mes y dia. y 30 de febrero no es una fecha
+	
+	if(!argv_fecha || !fecha || !datos_usuario)
+		return ST_ERROR_PUNTERO_NULO;
+
 	if (!convertir_a_numero_entero(argv_fecha, fecha))
 		return ST_ERROR_MES_INVALIDO;
 
 	if (*fecha < 0) 
 		return ST_ERROR_FECHA_INVALIDA;
 	
+	partir_fecha(fecha, datos_usuario);
+
+	if (datos_usuario->fecha.dia < CANT_MIN_DIA || datos_usuario->fecha.dia > CANT_MAX_DIA)
+		return ST_ERROR_DIA_INVALIDO;
+	if (datos_usuario->fecha.mes < CANT_MIN_MES || datos_usuario->fecha.mes > CANT_MAX_MES)
+		return ST_ERROR_MES_INVALIDO;
+	if (datos_usuario->fecha.anio < CANT_MIN_ANIO || datos_usuario->fecha.anio > CANT_MAX_ANIO)
+		return ST_ERROR_ANIO_INVALIDO;
+
 	return ST_OK;
 }
 
@@ -76,66 +88,60 @@ status_t validar_argumento_dia(char *argv_dia, int *dia) {
 }
 
 //NOMBRE DUDOSO, PROPENSO A CAMBIO
-status_t partir_fecha(int *fecha, int *dia, int *mes, int *anio) {
+status_t partir_fecha(int *fecha, metadata *datos_usuario) {
 
-	if (!fecha || !dia || !mes || !anio)
+	if (!fecha || !datos_usuario)
 		return ST_ERROR_PUNTERO_NULO;
 
-	*anio = *fecha / 10000;
-	*mes = (*fecha % 10000) / 100; //ver que sea del 1 al 12
-	*dia = (*fecha % 10000) % 100; // ver que sea del 0 al 31
-
-	printf("fecha %d\n", *fecha);
-	printf("año : %d\n", *anio);
-	printf("mes : %d\n", *mes);
-	printf("Dia : %d\n", *dia);
-
+	datos_usuario->fecha.anio = *fecha / 10000;
+	datos_usuario->fecha.mes = (*fecha % 10000) / 100; //ver que sea del 1 al 12
+	datos_usuario->fecha.dia = (*fecha % 10000) % 100; // ver que sea del 0 al 31
+	
 	return ST_OK;
 	
 }
 
-status_t cargar_fecha_por_omision (metadata * datos_usuario) {
+bool cargar_fecha_por_omision (metadata * datos_usuario) {
     
     time_t tiempo; 
     struct tm * fecha_actual; 
     tiempo = time(NULL); 
     fecha_actual = localtime(&tiempo);
 	
-	/* Validar algo ??
-	if(!())
-		return ST_ERROR_PUNTERO_NULO;
-	*/
-	datos_usuario->fecha.dia  = fecha_actual->tm_mday;
-	datos_usuario->fecha.mes  = fecha_actual->tm_mon;
-	datos_usuario->fecha.anio = fecha_actual->tm_year; //VER PORQUE me da desde 1900
+	if(!(datos_usuario))
+		return false;
 
-	return ST_OK;
+	datos_usuario->fecha.dia  = fecha_actual->tm_mday;
+	datos_usuario->fecha.mes  = (fecha_actual->tm_mon) + 1;
+	datos_usuario->fecha.anio = (fecha_actual->tm_year) + 1900; //VER PORQUE me da desde 1900
+
+	return true;
 }
 
-status_t cargar_nombre_por_omision(metadata *datos_usuario) {
+bool cargar_nombre_por_omision(metadata *datos_usuario) {
 
 	if (!datos_usuario)
-		return ST_ERROR_PUNTERO_NULO;
+		return false;
 
 	strcpy(datos_usuario->nombre, NOMBRE_POR_OMISION); //VER QUE NOMBRE PONER
 
 
-	return ST_OK;
+	return true;
 }
 
-bool cargar_hora_por_omision (horario_t *horario) {
+bool cargar_hora_por_omision (metadata *datos_usuario) {
     
 	time_t tiempo; 
     struct tm *hora; 
     tiempo = time(NULL); 
     hora = localtime(&tiempo);
 
-	if(!horario)
+	if(!datos_usuario)
 		return false;
 
-	horario -> segundos = hora -> tm_sec;
-	horario -> minuto = (hora -> tm_min) + 1;
-	horario -> hora = (hora ->tm_hour) + 1;
+	datos_usuario -> horario.segundos =(float)hora -> tm_sec;
+	datos_usuario -> horario.minuto = (hora -> tm_min) + 1;
+	datos_usuario -> horario.hora = (hora ->tm_hour) + 1;
 
 	return true;
 }
