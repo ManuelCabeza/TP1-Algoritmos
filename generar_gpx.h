bc1a7547392c7f8084ba4266fbe8d1e1a8c3adca
Tamara <tamaraespejoruiz@gmail.com>
2018-10-15 04:28:55 -0300

    Falta validar fecha

--- generar_gpx.h
+++ generar_gpx.h
@@ -1,14 +1,18 @@
+#ifndef GENERAR_GPX_H
+#define GENERAR_GPX_H
+
 #define MAX_LONG_NOMBRE 100
 
 typedef struct {
 	char nombre[MAX_LONG_NOMBRE];
-	// VER TIME.H para el tiempo
+	horario_t horario;
 } metadata;
 
 
 typedef enum {INICIAR, INICIAR_ENTER, FINAL, FINAL_ENTER} tipo_tag;
 
-void generar_gpx(gga * ggaptr, metadata * metptr);
+void generar_gpx(gga * ggaptr, metadata * metptr, fecha_t *fecha);
 
 void tag(char * strptr, tipo_tag tipo, size_t indentacion);
 
+#endif
