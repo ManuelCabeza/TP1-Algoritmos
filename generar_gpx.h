4de3b96a06edcfc71a7b5a6e3547cb298b576437
Tamara <tamaraespejoruiz@gmail.com>
2018-10-15 16:57:55 -0300

    Pase todos los define en el generar_gpx.h

--- generar_gpx.h
+++ generar_gpx.h
@@ -2,6 +2,32 @@
 #define GENERAR_GPX_H
 
 
+#define MSJ_GPX_1 "?xml version=\"1.0\" encoding=\" UTF-8\"?"
+#define MSJ_GPX_2 "gpx version=\"1.1\" creator=\"95.11 TP1 - Tracker\" xmlns=\"http://www.topografix.com/GPX/1/1\""
+#define CARACTER_TAG_INICIO '<'
+#define CARACTER_TAG_FINAL '>'
+#define CARACTER_TAG_FINALIZAR '/'
+#define CARACTER_INDENTACION '\t'
+
+#define INDENTACION_INICIAL 0
+#define INDENTACION_0 0 
+#define INDENTACION_1 1
+#define INDENTACION_2 2
+#define INDENTACION_3 3
+#define INDENTACION_4 4
+
+#define TAG_GPX "gpx"
+#define TAG_TRK "trk"
+#define TAG_TRKSEG "trkseg"
+#define TAG_TRKPT "trkpt"
+#define TAG_LATITUD "lat="
+#define TAG_LONGITUD "lon="
+#define TAG_ELEVACION "ele"
+#define TAG_TIEMPO "time"
+#define TAG_METADATA "metadata"
+#define TAG_NOMBRE "name"
+
+
 /* Esta se define de vardad aca
 #define MAX_LONG_NOMBRE 100
 
