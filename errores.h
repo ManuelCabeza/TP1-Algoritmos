5a1dd3553eeb93f24584ca6752739380ca3576c4
Tamara <tamaraespejoruiz@gmail.com>
2018-10-17 22:07:55 -0300

    Agregue la funcion imprimir_errores en eeste archivo

---
+++ errores.h
@@ -0,0 +1,8 @@
+#ifndef ERRORES_H
+#define ERRORES_H
+
+#include "verificaciones_main.h"
+
+void imprimir_errores(status_t estado);
+/* Dependiendo del estado que reciba, imprime el correspondiente mensaje de error. */
+#endif
