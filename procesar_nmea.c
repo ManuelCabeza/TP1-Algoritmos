6a1d1334f5939191d4590e42297ddb608d9284f3
Tamara <tamaraespejoruiz@gmail.com>
2018-10-25 22:55:05 -0300

    Puse las llaves que pidio el docente

--- procesar_nmea.c
+++ procesar_nmea.c
@@ -1,49 +1,42 @@
 #include "procesar_nmea.h"
-#include "estructuras.h"
+#include "main.h"
 #include "verificaciones_main.h"
 #include "generar_gpx.h"
-
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <time.h>
 
 procesar_t procesar_nmea(gga_t * ggaptr) {
 
 	char c; /*Variable auxiliar para no usar tolower mas de 2 veces.*/
 	char string[MAX_LONG_SEN];
-	char * strptr;
+	char * str;
 
 	long suma_verificacion;
 	float horario;
 	float longitud;
 	float latitud;
 
-	if (fgets(string, MAX_LONG_SEN,stdin) == NULL)
+	if (fgets(string, MAX_LONG_SEN,stdin) == NULL) {
 		return PR_FIN;
+	}
+	str = string;
 
-	strptr = string;
-
-	if (* strptr != CARACTER_INICIO_COMANDO)
+	if (* str != CARACTER_INICIO_COMANDO) {
 		return PR_ERR;
+	}
 
-
-	if ((strptr = strchr(string, CARACTER_SEPARACION_COMANDO)) != NULL) {
-		strptr++;
-		if ((horario = strtof(strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO)) { 
+	if ((str = strchr(string, CARACTER_SEPARACION_COMANDO)) != NULL) {
+		str++;
+		if ((horario = strtof(str, &str)) < 0 || ((* (str++)) != CARACTER_SEPARACION_COMANDO)) { 
 			return PR_ERR;
 		}
 	}
 
 	procesar_horario(ggaptr, horario);
 
-	if ((latitud = strtof(strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO)) { 
+	if ((latitud = strtof(str, &str)) < 0 || ((* (str++)) != CARACTER_SEPARACION_COMANDO)) { 
 		return PR_ERR;
 	}
 
-	c = tolower( * (strptr++));
+	c = tolower( * (str++));
 	if ((c != CARACTER_NORTE) && (c != CARACTER_SUR)){ 
 		return PR_ERR;
 	}
@@ -53,47 +46,47 @@ procesar_t procesar_nmea(gga_t * ggaptr) {
 
 
 
-	if ((longitud = strtof(++strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO))
+	if ((longitud = strtof(++str, &str)) < 0 || ((* (str++)) != CARACTER_SEPARACION_COMANDO)) {
 		return PR_ERR;
-
-	c = tolower( * (strptr++));
-	if ((c != CARACTER_ESTE) && (c != CARACTER_OESTE))
+	}
+	c = tolower( * (str++));
+	if ((c != CARACTER_ESTE) && (c != CARACTER_OESTE)) {
 		return PR_ERR;
-
+	}
 	/* Asigna valor de longitud a partir de formato dddmm.mmm */
 	ggaptr->longitud = (((int)longitud / 100) + ((longitud - 100 * ((int)longitud / 100)) / 60 )) * (c == CARACTER_OESTE ? -1 : 1);
 
-	if ((ggaptr->calidad_fix = strtol(++strptr, &strptr, 10)) < MIN_VALOR_FIX || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO) || (ggaptr->calidad_fix) > MAX_VALOR_FIX)
+	if ((ggaptr->calidad_fix = strtol(++str, &str, 10)) < MIN_VALOR_FIX || ((* (str++)) != CARACTER_SEPARACION_COMANDO) || (ggaptr->calidad_fix) > MAX_VALOR_FIX) {
 		return PR_ERR;
-
-	if ((ggaptr->cant_satelites = strtof(++strptr, &strptr)) < MIN_VALOR_CANT_SATELITES || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO) || (ggaptr->cant_satelites > MAX_VALOR_CANT_SATELITES))
+	}
+	if ((ggaptr->cant_satelites = strtof(++str, &str)) < MIN_VALOR_CANT_SATELITES || ((* (str++)) != CARACTER_SEPARACION_COMANDO) || (ggaptr->cant_satelites > MAX_VALOR_CANT_SATELITES)) {
 		return PR_ERR;
-
-	if ((ggaptr->hdop = strtof(++strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO))
+	}
+	if ((ggaptr->hdop = strtof(++str, &str)) < 0 || ((* (str++)) != CARACTER_SEPARACION_COMANDO)) {
 		return PR_ERR;
-
-	if ((ggaptr->elevacion = strtof(strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO))
+	}
+	if ((ggaptr->elevacion = strtof(str, &str)) < 0 || ((* (str++)) != CARACTER_SEPARACION_COMANDO)) {
 		return PR_ERR;
-
-	if (tolower(* strptr++) != CARACTER_UNIDAD)
+	}
+	if (tolower(* str++) != CARACTER_UNIDAD) {
 		return PR_ERR;
-
-	if ((ggaptr->sep_geo = strtof(++strptr, &strptr)) < 0 || ((* (strptr++)) != CARACTER_SEPARACION_COMANDO))
+	}
+	if ((ggaptr->sep_geo = strtof(++str, &str)) < 0 || ((* (str++)) != CARACTER_SEPARACION_COMANDO)) {
 		return PR_ERR;
-
-	if (tolower(* strptr) != CARACTER_UNIDAD)
+	}
+	if (tolower(* str) != CARACTER_UNIDAD) {
 		return PR_ERR;
-
-	if ((strptr = strrchr(string, CARACTER_SUMA_VER)) == NULL)
+	}
+	if ((str = strrchr(string, CARACTER_SUMA_VER)) == NULL) {
 		return PR_ERR;
+	}
+	suma_verificacion = strtol(++str, NULL, 16);
 
-	suma_verificacion = strtol( ++strptr, NULL, 16);
+	str = string + 1;
 
-	strptr = string + 1;
-
-	if (nmea_verificar_suma( strptr) != suma_verificacion)
+	if (nmea_verificar_suma( str) != suma_verificacion) {
 		return PR_ERR;
-	
+	}
 	return PR_OK;
 }
 
@@ -104,9 +97,9 @@ unsigned char nmea_verificar_suma(const char * sentencia) {
 
 	unsigned char suma = 0;
 
-	while ( * sentencia != CARACTER_SUMA_VER )
+	while ( * sentencia != CARACTER_SUMA_VER) {
 		suma ^= *sentencia++;
-
+	}
 	return suma;
 }
 
