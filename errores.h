10ce047fce9d19808559d7ca8b1874c83aa19ca4
Tamara <tamaraespejoruiz@gmail.com>
2018-10-25 22:52:25 -0300

    Pase los defines que correspondia aca

--- errores.h
+++ errores.h
@@ -4,6 +4,17 @@
 #include <stdio.h>
 #include "verificaciones_main.h"
 
+#define MSJ_ERROR_PREFIJO "Error"
+#define MSJ_ERROR_PUNTERO_NULO "Puntero nulo."
+#define MSJ_ERROR_FECHA_INVALIDA "La fecha ingresada es invalida."
+#define MSJ_ERROR_NOMBRE_INVALIDO "El nombre ingresado es invalido."
+#define MSJ_ERROR_DIA_INVALIDO "El dia ingresado es invalido."
+#define MSJ_ERROR_MES_INVALIDO "El mes ingresado es invalido."
+#define MSJ_ERROR_ANIO_INVALIDA "El año ingresado es invalido. "
+#define MSJ_ERROR_SEGUNDOS_INVALIDO "El segundo ingresado es invalido. "
+#define MSJ_ERROR_MINUTO_INVALIDO "El minuto ingresado es invalido. "
+#define MSJ_ERROR_HORA_INVALIDA "La hora ingresada es invalida. "
+#define MSJ_ERROR_ARG_INVALIDO "El argumento ingresado no es valido. "
 
 /* Dependiendo del estado que reciba, imprime el correspondiente mensaje de error. */
 void imprimir_errores(status_t estado);
