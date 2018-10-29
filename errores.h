5c4f9197e77062c2e92d31f93cfe6b948376c860
Tamara <tamaraespejoruiz@gmail.com>
2018-10-29 00:11:04 -0300

    Agregue correspondientes errores

--- errores.h
+++ errores.h
@@ -14,7 +14,8 @@
 #define MSJ_ERROR_SEGUNDOS_INVALIDO "El segundo ingresado es invalido. "
 #define MSJ_ERROR_MINUTO_INVALIDO "El minuto ingresado es invalido. "
 #define MSJ_ERROR_HORA_INVALIDA "La hora ingresada es invalida. "
-#define MSJ_ERROR_ARG_INVALIDO "El argumento ingresado no es valido. "
+#define MSJ_ERROR_CANT_ARG_INVALIDO "La cantidad de argumentos ingresados no es valida. "
+#define MSJ_ERROR_ARG_INVALIDO "El argumento ingresado es invalido. "
 
 /* Dependiendo del estado que reciba, imprime el correspondiente mensaje de error. */
 void imprimir_errores(status_t estado);
