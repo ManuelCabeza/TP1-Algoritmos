2b632f6a29d01f4e059ec42cdf55cbe053da98df
Tamara <tamaraespejoruiz@gmail.com>
2018-10-29 23:48:48 -0300

    Cambie el nombre de verificaciones_main a verificar_argumentos

--- errores.h
+++ errores.h
@@ -2,7 +2,7 @@
 #define ERRORES_H
 
 #include <stdio.h>
-#include "verificaciones_main.h"
+#include "verificar_argumentos.h"
 
 #define MSJ_ERROR_PREFIJO "Error"
 #define MSJ_ERROR_PUNTERO_NULO "Puntero nulo."
@@ -11,11 +11,14 @@
 #define MSJ_ERROR_DIA_INVALIDO "El dia ingresado es invalido."
 #define MSJ_ERROR_MES_INVALIDO "El mes ingresado es invalido."
 #define MSJ_ERROR_ANIO_INVALIDA "El año ingresado es invalido. "
+#define MSJ_ERROR_CANT_ARG_INVALIDO "La cantidad de argumentos ingresados no es valida. "
+#define MSJ_ERROR_ARG_INVALIDO "El argumento ingresado es invalido. "
+
+//Ver uso, creo que estan alpedo.
 #define MSJ_ERROR_SEGUNDOS_INVALIDO "El segundo ingresado es invalido. "
 #define MSJ_ERROR_MINUTO_INVALIDO "El minuto ingresado es invalido. "
 #define MSJ_ERROR_HORA_INVALIDA "La hora ingresada es invalida. "
-#define MSJ_ERROR_CANT_ARG_INVALIDO "La cantidad de argumentos ingresados no es valida. "
-#define MSJ_ERROR_ARG_INVALIDO "El argumento ingresado es invalido. "
+
 
 /* Dependiendo del estado que reciba, imprime el correspondiente mensaje de error. */
 void imprimir_errores(status_t estado);
