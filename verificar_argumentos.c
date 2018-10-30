4c5adf5088b4bef6b0bc4487adc852df2930e2be
Tamara <tamaraespejoruiz@gmail.com>
2018-10-29 23:50:35 -0300

    Cambie el nombre de verificaciones_main a verificar_argumentos

---
+++ verificar_argumentos.c
@@ -0,0 +1,261 @@
+#include "verificar_argumentos.h"
+#include "generar_gpx.h"
+#include "procesar_nmea.h"
+#include "main.h"
+#include "errores.h"
+
+#define MAX_CANT_ARG 10
+
+status_t procesar_argumentos(int argc, char * argv[], metadata_t * datos_usuario) {
+
+	const char * arg_validos[] = { ARG_VALIDO_AYUDA, ARG_VALIDO_AYUDA_V ,
+								   ARG_VALIDO_NOMBRE, ARG_VALIDO_NOMBRE_V,
+								   ARG_VALIDO_FECHA, ARG_VALIDO_FECHA_V,
+								   ARG_VALIDO_ANIO, ARG_VALIDO_ANIO_V,
+								   ARG_VALIDO_MES, ARG_VALIDO_MES_V,
+								   ARG_VALIDO_DIA, ARG_VALIDO_DIA_V
+						          };
+
+	int i, j;
+	status_t estado;
+	bool esta_fecha = false;
+// La uso como bandera indicadora para ver si esta el argumeto -f o --format
+
+	if (!argv || !datos_usuario) { 
+		return ST_ERROR_PUNTERO_NULO;
+	}
+
+	if (argc > MAX_CANT_ARG) {
+		return ST_ERROR_CANT_ARG_INVALIDO;
+	}
+
+	for (i = 1; i < argc; i++) {
+		for (j = 0; j < MAX_CANT_ARG_VALIDOS; j++) {
+			if (strcmp(argv[i], arg_validos[j]) == 0) {
+				j = j / 2;
+				switch (j) {
+					case ARG_AYUDA:
+						return ST_PEDIR_AYUDA;
+						break;
+					case ARG_NOMBRE:
+						i++;
+						estado = validar_argumento_nombre(argv[i], datos_usuario->nombre);
+						break;
+					case ARG_FECHA:
+						esta_fecha = true;
+						i++;
+						estado = validar_argumento_fecha(argv[i], &(datos_usuario->fecha));
+						break;
+					case ARG_ANIO:
+						i++;
+						if (esta_fecha) { 
+							break;
+						}
+						estado = validar_argumento_anio(argv[i], &(datos_usuario->fecha).anio);
+						break;
+					case ARG_MES:
+						i++;
+						if (esta_fecha) {
+							break;
+						}
+						estado = validar_argumento_mes(argv[i], &(datos_usuario->fecha).mes);
+						break;
+					case ARG_DIA:
+						i++;
+						if (esta_fecha) { 
+							break;
+						}
+						estado = validar_argumento_dia(argv[i], &(datos_usuario->fecha).dia);
+						break;
+			
+				}
+
+				if (estado != ST_OK) {
+					return estado;
+				}
+
+			}
+//			else {
+//				return ST_ERROR_ARG_INVALIDO;
+//			}
+
+		}
+	}
+
+	return ST_OK;
+}
+
+bool convertir_a_numero_entero(char *cadena, int *resultado) {
+
+	char *perr = NULL;
+
+	if (!cadena|| !resultado) { 
+		return false;
+	}
+	*resultado = strtol(cadena, &perr, 10);
+
+	if (*perr != '\0') { 
+		return false;
+	}
+	return true;
+}
+
+status_t validar_argumento_nombre(char *argv_nombre, char *nombre) {
+//que no este vacio, y que no empieze con un signo menos.
+
+//argv  no sea mas largo que los caracteres que pueda copiar.
+	size_t largo;
+	if (!argv_nombre|| !nombre) { 
+		return ST_ERROR_PUNTERO_NULO;
+	}
+
+	largo = (strlen(argv_nombre) + 1);
+
+	if (largo > CANT_MAX) {
+		return ST_ERROR_NOMBRE_INVALIDO;
+	}
+	strcpy(nombre, argv_nombre);
+
+	return ST_OK;
+}
+
+status_t validar_argumento_fecha(char *argv_fecha, fecha_t *fecha) {
+
+	int fecha_por_comando;
+	
+	if(!argv_fecha || !fecha) {
+		return ST_ERROR_PUNTERO_NULO;
+	}
+	if (!convertir_a_numero_entero(argv_fecha, &fecha_por_comando)) {
+		return ST_ERROR_FECHA_INVALIDA;
+	}
+	if (fecha_por_comando < CANT_MIN_FECHA) {
+		return ST_ERROR_FECHA_INVALIDA;
+	}
+	partir_fecha(fecha_por_comando, fecha);
+
+	if (fecha->dia < CANT_MIN_DIA || fecha->dia > CANT_MAX_DIA) {
+		return ST_ERROR_DIA_INVALIDO;
+	}
+	if (fecha->mes < CANT_MIN_MES || fecha->mes > CANT_MAX_MES) {
+		return ST_ERROR_MES_INVALIDO;
+	}	
+	if (fecha->anio < CANT_MIN_ANIO || fecha->anio > CANT_MAX_ANIO) {
+		return ST_ERROR_ANIO_INVALIDO;
+	}
+	return ST_OK;
+}
+
+status_t validar_argumento_mes(char *argv_mes, int * mes) {
+
+	if(!argv_mes || !mes) {
+		return ST_ERROR_PUNTERO_NULO;
+	}
+	if (!convertir_a_numero_entero(argv_mes, mes)) {
+		return ST_ERROR_MES_INVALIDO;
+	}
+	if (*mes < CANT_MIN_MES || *mes > CANT_MAX_MES) {
+		return ST_ERROR_MES_INVALIDO;
+	}
+
+	return ST_OK;
+}
+
+status_t validar_argumento_anio(char *argv_anio, int *anio) {
+
+	if(!argv_anio || !anio) {
+		return ST_ERROR_PUNTERO_NULO;
+	}
+	if (!convertir_a_numero_entero(argv_anio, anio)) {
+		return ST_ERROR_ANIO_INVALIDO;
+	}
+	if (*anio < CANT_MIN_ANIO || *anio > CANT_MAX_ANIO) {
+		return ST_ERROR_ANIO_INVALIDO;
+	}
+
+	return ST_OK;
+}
+
+status_t validar_argumento_dia(char *argv_dia, int *dia) {
+	
+	if(!argv_dia || !dia) {
+		return ST_ERROR_PUNTERO_NULO;
+	}
+	if (!convertir_a_numero_entero(argv_dia, dia)) {
+		return ST_ERROR_DIA_INVALIDO;
+	}
+	if (*dia < CANT_MIN_DIA || *dia > CANT_MAX_DIA) {
+		return ST_ERROR_DIA_INVALIDO;
+	}
+
+	return ST_OK;
+}
+
+/*Recibe a fecha:aaaammdd y carga a la estructura año = aaaa, mes = mm y dia = dd */
+status_t partir_fecha(int fecha_actual, fecha_t *fecha) {
+
+	if (!fecha) {
+		return ST_ERROR_PUNTERO_NULO;
+	}
+	fecha->anio = fecha_actual / 10000;
+	fecha->mes = (fecha_actual % 10000) / 100;
+	fecha->dia = (fecha_actual % 10000) % 100;
+
+	return ST_OK;
+}
+
+bool cargar_fecha_por_omision(fecha_t *fecha) {
+
+    time_t tiempo;
+    struct tm * fecha_actual;
+    tiempo = time(NULL);
+    fecha_actual = localtime(&tiempo);
+
+	if(!fecha) {
+		return false;
+	}
+	fecha->dia  = fecha_actual->tm_mday;
+	fecha->mes  = (fecha_actual->tm_mon) + AJUSTE_DE_NUM;
+	fecha->anio = (fecha_actual->tm_year) + ANIO_DE_LINUX;
+
+	return true;
+}
+
+bool cargar_nombre_por_omision(char *nombre) { //metadata_t *datos_usuario
+
+	size_t largo;
+
+	if (!nombre) {
+		return false;
+	}
+	largo = (strlen(nombre) + 1);
+
+	if (largo > CANT_MAX) {
+		return ST_ERROR_NOMBRE_INVALIDO;
+	}
+	strcpy(nombre, NOMBRE_POR_OMISION);
+
+	return true;
+}
+
+bool cargar_hora_por_omision (horario_t *horario) {
+
+	time_t tiempo;
+    struct tm *hora;
+    tiempo = time(NULL);
+    hora = localtime(&tiempo);
+
+	if(!horario) {
+		return false;
+	}
+	horario->segundos = (float)hora -> tm_sec;
+	horario->minuto = (hora -> tm_min) + AJUSTE_DE_NUM;
+	horario->hora = (hora ->tm_hour) /*+ AJUSTE_DE_NUM*/;
+
+	return true;
+}
+
+void imprimir_ayuda(void) {
+
+	printf("%s\n", MSJ_IMPRIMIR_AYUDA);
+}
