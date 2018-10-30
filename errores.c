732425393eec1a573d5678f2c877d4382adaa8a9
Tamara <tamaraespejoruiz@gmail.com>
2018-10-30 01:48:41 -0300

    Compila, falta validar si pasa todos los casos. Hecho lo de errores en 'un diccionario' y validar argumento invalido

--- errores.c
+++ errores.c
@@ -2,6 +2,21 @@
 #include "errores.h"
 #include "verificar_argumentos.h"
 
+
+void imprimir_error(status_t estado) {
+
+	const char * msj_error[] = { "", "",
+								MSJ_ERROR_PUNTERO_NULO, MSJ_ERROR_FECHA_INVALIDA,
+							    MSJ_ERROR_NOMBRE_INVALIDO, MSJ_ERROR_DIA_INVALIDO,
+								MSJ_ERROR_MES_INVALIDO, MSJ_ERROR_ANIO_INVALIDA,
+								MSJ_ERROR_CANT_ARG_INVALIDO, MSJ_ERROR_ARG_INVALIDO
+							   };
+
+
+	fprintf(stderr, "%s : %d   %s\n", MSJ_ERROR_PREFIJO, estado, msj_error[estado]);
+}
+
+/*
 void imprimir_errores(status_t estado) {
 
 	switch (estado) {
@@ -36,3 +51,4 @@ void imprimir_errores(status_t estado) {
 	}
 
 }
+*/
