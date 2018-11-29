088fdfc74310edaee1ead58357b6ffd4e8174183
Tamara <tamaraespejoruiz@gmail.com>
2018-11-29 13:54:50 -0300

    Aca esta lo del log por ahora

--- errores.c
+++ errores.c
@@ -3,40 +3,31 @@
 
 /*
 Forma de imprimir en el LOG
-
 año-mes-dia horas:minutos:segundos [ERROR] mensaje
 año-mes-dia horas:minutos:segundos [WARN] mensaje
 año-mes-dia horas:minutos:segundos [DEBUG] mensaje
 */
 
-//LOGS es lo mismo para ver stdout
-
-
-//PARA MODIFICAR UNA ESTRUCTURA NO HACE FALTA PASAR DOS NIVELES DE PUNTERO
-void imprimir_errores_log (status_t *estado, FILE *archivo_log) { //Pasa una estructura
+void imprimir_msj_log (status_t estado, FILE **archivo_log/*, gps_t *datos_satelite*/) { //Pasa una estructura
 
 	const char * msj_error[] = { MSJ_VACIO, MSJ_VACIO,
 								MSJ_ERROR_PUNTERO_NULO, MSJ_ERROR_NOMBRE_INVALIDO, 
 								MSJ_ERROR_PROTOCOLO,
 							    MSJ_ERROR_ARCHIVO_ENTRADA, MSJ_ERROR_ARCHIVO_SALIDA,
 								MSJ_ERROR_ARCHIVO_LOG, MSJ_ERROR_CANTIDAD_MSJ,
+								MSJ_ERROR_CANTIDAD_ARGUMENTOS_INVALIDOS,
 								MSJ_ERROR_ARG_INVALIDO, MSJ_ERROR_LECTURA
 							   }; 
 /*Tiene que tener el mismo orden de status_t para que sea un diccionario*/
 
-	if (archivo_log == stderr) { 
-	fprintf(stderr, "%s : %s\n", MSJ_ERROR_PREFIJO, msj_error[*estado]);
-	}
-
-	fprintf(archivo_log, "%s : %s\n", MSJ_ERROR_PREFIJO, msj_error[*estado]);
-
+	/*fprintf(*archivo_log, "%d-%d-%d %d:%d:%d [] %s\n", datos_satelite->fecha.anio, 
+														 datos_satelite->fecha.mes,
+														 datos_satelite->fecha.dia,
+														 datos_satelite->horario.hora,
+														 datos_satelite->horario.minuto,
+														 datos_satelite->horario.segundos,
+														 	)*/
+	fprintf(*archivo_log, "%s : %s\n", MSJ_ERROR_PREFIJO, msj_error[estado]);
 }
 
-/*
-status_t procesar_logs(logs_t *log, file **archivo_log, //las estructuras ) {
-
-    fprintf(stdout, "%d%d\n", 7, 34) // o nombre del archivo
-}
-
-*/
 
--- errores.h
+++ errores.h
@@ -6,23 +6,23 @@
 
 #include "verificar_argumentos.h"
 
-
 #define MSJ_ERROR_PREFIJO "Error"
 #define MSJ_VACIO ""
 #define MSJ_ERROR_PUNTERO_NULO "Puntero nulo."
 #define MSJ_ERROR_NOMBRE_INVALIDO "El nombre ingresado es invalido." //lo voy a eliminar!
-#define MSJ_ERROR_PROTOCOLO "El protocolo ingresad es invalido."
+#define MSJ_ERROR_PROTOCOLO "El protocolo ingresado es invalido."
 #define MSJ_ERROR_ARCHIVO_ENTRADA "El archivo de entrada es invalido." //Tengo que deci que no existe?
 #define MSJ_ERROR_ARCHIVO_SALIDA "El archivo de salida es invalido."
 #define MSJ_ERROR_ARCHIVO_LOG "El archivo log es invalido."
 #define MSJ_ERROR_CANTIDAD_MSJ "La cantidad de mensajes no es valido."
-#define MSJ_ERROR_ARG_INVALIDO "El argumento ingresado es invalido, pruebe ingresando el comando -h para obtener ayuda." 
+#define MSJ_ERROR_CANTIDAD_ARGUMENTOS_INVALIDOS "La cantidad de argumentos ingresados es invalido."
 
+#define MSJ_ERROR_ARG_INVALIDO "El argumento ingresado es invalido, pruebe ingresando el comando -h para obtener ayuda." 
 
 #define MSJ_ERROR_LECTURA "No se pudo leer el archivo"
 /* Dependiendo del estado que reciba, imprime el correspondiente mensaje de error. */
 
 
-void imprimir_errores_log(status_t *estado, FILE *archivo_log);
+void imprimir_msj_log(status_t estado, FILE **archivo_log);
 
 #endif
